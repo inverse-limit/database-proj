59e4e9f12024a7b65d1d497dd12abf505fda7ec6
ZJJ <zjj_0707@outlook.com>
2020-07-08 17:29:35 +0800

    小类框加大

--- database.py
+++ database.py
@@ -3,7 +3,7 @@ import pyodbc
 
 class Database:
     def __init__(self):
-       self.cnxn = pyodbc.connect('DRIVER={SQL Server};SERVER=w32s719895.qicp.vip,17865;\
+       self.cnxn = pyodbc.connect('DRIVER={SQL Server};SERVER=personal2.shenzhuo.vip,18140;\
                                    DATABASE=online_bookshop;UID=online_bookshop_m;PWD=dbproject')
 
     def register_function(self, account, pswd, name, telephone, email, invit, checkpswd):
